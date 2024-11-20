2040d926529df5521edd2d945bfa2cff9ec7fcd5
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2024-11-20 09:45:03 -0500

    implemented functionality to properly display digits being clicked on the calculator screen

--- js/index.js
+++ js/index.js
@@ -1,7 +1,46 @@
+let storage = '0';
+const answerDisplay = document.querySelector('.answer-display');
+
+//this function is to write numbers, clear, or back
+function operandButtonClick(value) {
+    if (!(isNaN(parseInt(value)))) {
+        determineNumber(value);
+    }
+}
+
+//this function is for each of the four operations or equal sign
+function operationButtonClick(value) {
+
+}
+
+//this is to keep track of the number
+function determineNumber(value) {
+    if (storage === '0') {
+        storage = value;
+    } else {
+        storage += value;
+    }
+    rerender();
+}
+
+function performMath(number, operation) {
+
+}
+
 function init() {
+    console.log('hi')
     document.querySelector('.grey-operands')
             .addEventListener("click", function(event) {
-                
+                operandButtonClick(event.target.innerText);
             })
-    document.querySelector('.')
+    document.querySelector('.operations')
+            .addEventListener("click", function(event) {
+                operationButtonClick(event.target.innerText);
+            })
 }
+
+function rerender() {
+    answerDisplay.innerText = storage;
+}
+
+init();
