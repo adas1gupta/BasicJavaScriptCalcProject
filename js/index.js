974b6f38dbfa53645f55be1bd955b0ccd29ebf3a
adas1gupta <dasgupta1arjun@gmail.com>
2024-11-19 22:04:44 -0500

    creating the JS file now

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function init() {
+    document.querySelector('.grey-operands')
+            .addEventListener("click", function(event) {
+                
+            })
+    document.querySelector('.')
+}
