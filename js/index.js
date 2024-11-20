262de06d41b4ec1840ee802a043271519baa7968
adas1gupta <dasgupta1arjun@gmail.com>
2024-11-20 13:27:42 -0500

    javascript finished. project finished.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 let storage = '0';
 const answerDisplay = document.querySelector('.answer-display');
+let storageArray = [];
 
 //this function is to write numbers, clear, or back
 function operandButtonClick(value) {
     if (!(isNaN(parseInt(value)))) {
         determineNumber(value);
+    } else if (value === 'C') {
+        storage = '0';
+        rerender();
+    } else {
+        back();
     }
 }
 
 //this function is for each of the four operations or equal sign
 function operationButtonClick(value) {
-
+    storageArray.push(storage);
+    if (value === '=') {
+        performMath();
+    }
+    else {
+        storageArray.push(value);
+        storage = '0';
+    }
 }
 
 //this is to keep track of the number
@@ -23,12 +36,46 @@ function determineNumber(value) {
     rerender();
 }
 
-function performMath(number, operation) {
+//back button implementation
+function back() {
+    if (storage === '0') {
+        storage = '0'
+    }
+    else {
+        storage = storage.slice(0, -1);
+    }
+    rerender();
+}
 
+//performing the math
+function performMath() {
+    if (storageArray.length === 3) {
+        let firstOperand = storageArray.pop();
+        let operation = storageArray.pop();
+        let secondOperand = storageArray.pop();
+        switch (operation) {
+            case '÷':
+                if (firstOperand === '0') {
+                    storage = 'undefined';
+                } else {
+                    storage = (parseInt(secondOperand) / parseInt(firstOperand)).toString();
+                }
+                break;
+            case '×':
+                storage = (parseInt(secondOperand) * parseInt(firstOperand)).toString();
+                break;
+            case '-':
+                storage = (parseInt(secondOperand) - parseInt(firstOperand)).toString();
+                break;
+            case '+':
+                storage = (parseInt(secondOperand) + parseInt(firstOperand)).toString();
+                break;
+        }
+    }
+    rerender();
 }
 
 function init() {
-    console.log('hi')
     document.querySelector('.grey-operands')
             .addEventListener("click", function(event) {
                 operandButtonClick(event.target.innerText);
